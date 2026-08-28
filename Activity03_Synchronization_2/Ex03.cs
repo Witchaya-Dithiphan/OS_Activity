26f1edc6d0d3aa45c9c3c636bdfb1ca828139d61
Poomwwe <poom.witchaya@gmail.com>
2026-08-29 02:36:42 +0700

    chore(Activity03-2): initial the file provied by instructor

---
+++ Activity03_Synchronization_2/Ex03.cs
@@ -0,0 +1,44 @@
+﻿//Base program
+using System;
+using System.Threading;
+
+namespace Ex03
+{
+    class Program
+    {
+        private static string x = "";
+        private static int exitflag = 0;
+
+        static void ThReadX(object i)
+        {
+            while (exitflag == 0)
+            {
+                Console.WriteLine("Thread-{0} : X = {1}", i, x);
+            }
+            Console.WriteLine("Thread {0} exit", i);
+        }
+
+        static void ThWriteX()
+        {
+            string xx = "";
+            while (exitflag == 0)
+            {
+                Console.WriteLine("Input: ");
+                xx = Console.ReadLine();
+                if (xx == "exit")
+                    exitflag = 1;
+                else
+                    x = xx;
+            }
+        }
+
+        static void Main()
+        {
+            Thread A = new Thread(ThReadX);
+            Thread B = new Thread(ThWriteX);
+
+            A.Start(1);
+            B.Start();
+        }
+    }
+}
