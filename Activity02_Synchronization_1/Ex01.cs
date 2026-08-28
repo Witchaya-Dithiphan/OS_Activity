cf4bec334ca0752f1b2fd7475475491795d0f1f2
Poomwwe <poom.witchaya@gmail.com>
2026-08-29 01:56:07 +0700

    chore(Activity02-1): Initial the files provided by instructor

---
+++ Activity02_Synchronization_1/Ex01.cs
@@ -0,0 +1,41 @@
+﻿// base program no thread
+using System;
+using System.Diagnostics;
+
+namespace ex01
+{
+    class Program
+    {
+        private static int sum = 0;
+
+        static void plus()
+        {
+            int i;
+            for (i = 1; i < 1000001; i++)
+            {
+                sum += i;
+            }
+        }
+
+        static void minus()
+        {
+            int i;
+            for (i = 0; i < 1000000; i++)
+            {
+                sum -= i;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Stopwatch sw = new Stopwatch();
+            Console.WriteLine("Start...");
+            sw.Start();
+            plus();
+            minus();
+            sw.Stop();
+            Console.WriteLine("sum = {0}", sum);
+            Console.WriteLine("Time used: " + sw.ElapsedMilliseconds.ToString() + "ms");
+        }
+    }
+}
